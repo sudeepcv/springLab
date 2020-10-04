e4c16617602ae65608e99a56ab0cec321b763071
sudeep cv <sudeep.cv@piserve.com>
2020-10-04 11:13:26 +0530

    spring boot react starter

---
+++ springboot/spring-boot-reactjs-starter/src/main/java/com/sudeepcv/springbootreactjsstarter/SpringBootReactjsStarterApplication.java
@@ -0,0 +1,13 @@
+package com.sudeepcv.springbootreactjsstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactjsStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactjsStarterApplication.class, args);
+	}
+
+}
---
+++ springboot/spring-boot-reactjs-starter/src/test/java/com/sudeepcv/springbootreactjsstarter/SpringBootReactjsStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudeepcv.springbootreactjsstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactjsStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
